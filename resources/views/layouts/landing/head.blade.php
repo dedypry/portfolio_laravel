8eba3e57d9f14d812410998ff73baf70ffe5149b
dedy priyatna <57609978+dedypry@users.noreply.github.com>
2025-05-12 23:19:03 +0700

    update

--- resources/views/layouts/landing/head.blade.php
+++ resources/views/layouts/landing/head.blade.php
@@ -17,12 +17,12 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
 
     <!-- Libraries Stylesheet -->
-    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
-    <link href="lib/lightbox/css/lightbox.min.css" rel="stylesheet">
-    <link href="lib/animate/animate.min.css" rel="stylesheet">
+    <link href="{{ asset('landing/lib/owlcarousel/assets/owl.carousel.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('landing/lib/lightbox/css/lightbox.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('landing/lib/animate/animate.min.css') }}" rel="stylesheet">
 
     <!-- Customized Bootstrap Stylesheet -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="{{ asset('landing/css/bootstrap.min.css') }}" rel="stylesheet">
 
     <!-- Template Stylesheet -->
-    <link href="css/style.css" rel="stylesheet">
+    <link href="{{ asset('landing/css/style.css') }}" rel="stylesheet">
--- resources/views/layouts/landing/script.blade.php
+++ resources/views/layouts/landing/script.blade.php
@@ -1,14 +1,14 @@
  <!-- JavaScript Libraries -->
  <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
- <script src="lib/wow/wow.min.js"></script>
- <script src="lib/typed/typed.min.js"></script>
- <script src="lib/easing/easing.min.js"></script>
- <script src="lib/waypoints/waypoints.min.js"></script>
- <script src="lib/counterup/counterup.min.js"></script>
- <script src="lib/owlcarousel/owl.carousel.min.js"></script>
- <script src="lib/isotope/isotope.pkgd.min.js"></script>
- <script src="lib/lightbox/js/lightbox.min.js"></script>
+ <script src="{{ asset('landing/lib/wow/wow.min.js') }}"></script>
+ <script src="{{ asset('landing/lib/typed/typed.min.js') }}"></script>
+ <script src="{{ asset('landing/lib/easing/easing.min.js') }}"></script>
+ <script src="{{ asset('landing/lib/waypoints/waypoints.min.js') }}"></script>
+ <script src="{{ asset('landing/lib/counterup/counterup.min.js') }}"></script>
+ <script src="{{ asset('landing/lib/owlcarousel/owl.carousel.min.js') }}"></script>
+ <script src="{{ asset('landing/lib/isotope/isotope.pkgd.min.js') }}"></script>
+ <script src="{{ asset('landing/lib/lightbox/js/lightbox.min.js') }}"></script>
 
  <!-- Template Javascript -->
- <script src="js/main.js"></script>
+ <script src="{{ asset('landing/js/main.js') }}"></script>
