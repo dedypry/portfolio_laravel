5059bb5c2b2abf1ae2971c897396a33d177ad00e
dedy priyatna <57609978+dedypry@users.noreply.github.com>
2025-05-18 22:12:27 +0700

    update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,10 @@
 
 namespace App\Providers;
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Str;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -12,7 +14,35 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        URL::macro('alternateHasCorrectSignature', function (Request $request, $absolute = true, array $ignoreQuery = []) {
+            $ignoreQuery[] = 'signature';
+
+            $absoluteUrl = url($request->path());
+            $url = $absolute ? $absoluteUrl : '/' . $request->path();
+
+            $queryString = collect(explode('&', (string) $request->server->get('QUERY_STRING')))
+                ->reject(fn($parameter) => in_array(Str::before($parameter, '='), $ignoreQuery))
+                ->join('&');
+
+            $original = rtrim($url . '?' . $queryString, '?');
+
+            $key = config('app.key');
+            if (empty($key)) {
+                throw new \RuntimeException('Application key is not set.');
+            }
+
+            $signature = hash_hmac('sha256', $original, $key);
+            return hash_equals($signature, (string) $request->query('signature', ''));
+        });
+
+        URL::macro('alternateHasValidSignature', function (Request $request, $absolute = true, array $ignoreQuery = []) {
+            return URL::alternateHasCorrectSignature($request, $absolute, $ignoreQuery)
+                && URL::signatureHasNotExpired($request);
+        });
+
+        Request::macro('hasValidSignature', function ($absolute = true, array $ignoreQuery = []) {
+            return URL::alternateHasValidSignature($this, $absolute, $ignoreQuery);
+        });
     }
 
     /**
