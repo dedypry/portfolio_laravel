ecdb1a540ffaf140a048cfbe76da98151164398b
dedy priyatna <57609978+dedypry@users.noreply.github.com>
2025-05-18 17:43:47 +0700

    update

--- resources/views/landing/components/portfolio.blade.php
+++ resources/views/landing/components/portfolio.blade.php
@@ -7,9 +7,9 @@
                     <ul class="list-inline mb-4" id="portfolio-flters">
                         <li class="btn btn-secondary active" data-filter="*"><i class="fa fa-star me-2"></i>All</li>
                         @foreach ($portfolio as $port)
-
-                        <li class="btn btn-secondary" data-filter=".{{$port->slug}}"><i class="fa {{$port->icon}} me-2"></i>{{$port->title}}
-                        </li>
+                            <li class="btn btn-secondary" data-filter=".{{ $port->slug }}"><i
+                                    class="fa {{ $port->icon }} me-2"></i>{{ $port->title }}
+                            </li>
                         @endforeach
                         {{-- <li class="btn btn-secondary" data-filter=".second"><i
                                 class="fa fa-mobile-alt me-2"></i>Development</li> --}}
@@ -22,19 +22,21 @@
                         $images = is_string($app->img_url) ? json_decode($app->img_url, true) : $app->img_url;
                     @endphp
 
-                    @foreach ($images as $img)
-                        <div class="col-md-6 mb-4 portfolio-item {{$app->slug}}">
-                            <div class="position-relative overflow-hidden mb-2">
-                                <img class="img-fluid w-100" style="height: 150px" src="{{ asset('storage/'.$img) }}"
-                                    alt="">
-                                <div class="portfolio-btn d-flex align-items-center justify-content-center">
-                                    <a href="{{ asset('storage/'.$img) }}" data-lightbox="portfolio">
-                                        <i class="bi bi-plus text-light"></i>
-                                    </a>
+                    @if ($images != null && count($images) > 0)
+                        @foreach ($images as $img)
+                            <div class="col-md-6 mb-4 portfolio-item {{ $app->slug }}">
+                                <div class="position-relative overflow-hidden mb-2">
+                                    <img class="img-fluid w-100" style="height: 150px"
+                                        src="{{ asset('storage/' . $img) }}" alt="">
+                                    <div class="portfolio-btn d-flex align-items-center justify-content-center">
+                                        <a href="{{ asset('storage/' . $img) }}" data-lightbox="portfolio">
+                                            <i class="bi bi-plus text-light"></i>
+                                        </a>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    @endforeach
+                        @endforeach
+                    @endif
                 @endforeach
 
             </div>
